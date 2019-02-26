6826e4199d5cf89d6f6d41989d08c978da2004f0
lihaichen <li8303@163.com>
2019-02-26 10:08:28 +0800

    加入协议接口

---
+++ common/protocol.go
@@ -0,0 +1,14 @@
+package common
+
+// IProtocol 协议标准接口
+type IProtocol interface {
+	GetName() string
+	Encode(config map[string]interface{}) (data []byte, err error)
+	Decode(data []byte) (value map[string]interface{}, err error)
+	Verify(data []byte) (err error)
+}
+// AProtocol 协议结构体
+type AProtocol struct {
+	// 编码时存入，方便解码或者校验使用
+	config map[string]interface{}
+}
